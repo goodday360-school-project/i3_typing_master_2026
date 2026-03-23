5f83090c7b8233391f550b9f90691dc873b6a311
mengan-tech <menganhan763@gmail.com>
2026-03-23 10:24:11 +0700

    reset password

--- reset_password_screen.java
+++ reset_password_screen.java
@@ -3,7 +3,8 @@ public class reset_password_screen {
         System.out.println("welcome to the reset password screen");
         System.out.println("Username: ");
         System.out.println("Email: ");
-        System.out.println("reset password or login");
+        System.out.println("reset password or login"); 
         System.out.println("choose an option: - ");
+        
     }
 }
